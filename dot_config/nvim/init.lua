ec72a138e2e4fb621afcee2e54b98067c7623028
Miguel Fuentes <miguelfuentes@miguels-mbp.home>
2024-12-08 22:11:20 +0100

    fix: starhsip default

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -543,7 +543,7 @@ require('lazy').setup({
       -- for you, so that they are available from within Neovim.
       local ensure_installed = vim.tbl_keys(servers or {})
       vim.list_extend(ensure_installed, {
-        -- 'stylua', -- Used to format Lua code
+        'stylua', -- Used to format Lua code
         -- -- 'tsserver',
         -- 'eslint',
         -- 'rust_analyzer',
@@ -554,23 +554,23 @@ require('lazy').setup({
         -- 'pylsp',
         -- 'marksman',
 
-        'bashls',
-        'gopls',
-        'docker_compose_language_service',
-        'dockerls',
-        'eslint',
-        'html',
-        'jsonls',
-        'lua_ls',
-        'marksman',
-        'php-cs-fixer',
-        'prettier',
-        'pylsp',
-        'rust_analyzer',
-        'stylua',
-        'tailwindcss',
-        'ts_ls',
-        'yamlls',
+        -- 'bashls',
+        -- 'gopls',
+        -- 'docker_compose_language_service',
+        -- 'dockerls',
+        -- 'eslint',
+        -- 'html',
+        -- 'jsonls',
+        -- 'lua_ls',
+        -- 'marksman',
+        -- 'php-cs-fixer',
+        -- 'prettier',
+        -- 'pylsp',
+        -- 'rust_analyzer',
+        -- 'stylua',
+        -- 'tailwindcss',
+        -- 'ts_ls',
+        -- 'yamlls',
       })
       require('mason-tool-installer').setup { ensure_installed = ensure_installed }
 
