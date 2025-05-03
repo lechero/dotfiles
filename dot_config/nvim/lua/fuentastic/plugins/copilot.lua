ef8d80b7bf9f5bcc0a6a03da0d81232741428295
Miguel Fuentes <miguelfuentes@mac.home>
2025-05-03 22:19:07 +0200

    wip: copilot cmp tries

--- dot_config/nvim/lua/fuentastic/plugins/copilot.lua
+++ dot_config/nvim/lua/fuentastic/plugins/copilot.lua
@@ -1,56 +1,68 @@
+-- lua/fuentastic/plugins/copilot.lua
 return {
-	-- 1. Copilot.lua plugin (GitHub Copilot client)
 	"zbirenbaum/copilot.lua",
-	event = "InsertEnter", -- delay loading until Insert mode for efficiency [oai_citation:1‡neovimcraft.com](https://neovimcraft.com/plugin/zbirenbaum/copilot.lua/#:~:text=Because%20the%20copilot%20server%20takes,For%20example)
-	cmd = "Copilot", -- also allow manual `:Copilot` commands
-	build = ":Copilot auth", -- (optional) automatically authenticate on first install
+	event = "InsertEnter",
+	cmd = "Copilot",
+	build = ":Copilot auth",
 	opts = {
-		suggestion = {
-			enabled = true,
-			auto_trigger = true, -- auto-show suggestions as you type [oai_citation:2‡lazyvim.github.io](https://lazyvim.github.io/extras/ai/copilot#:~:text=opts%20%3D%20,)
-			debounce = 75,
-			keymap = {
-				accept = "<Tab>", -- use <Tab> to accept suggestion [oai_citation:3‡gist.github.com](https://gist.github.com/haandol/c2ff89706fa8af6edf1bb2bb0c1fe3ba#:~:text=suggestion%20%3D%20,)
-				accept_word = false,
-				accept_line = false,
-				next = "<M-]>",
-				prev = "<M-[>",
-				dismiss = "<C-]>",
+		-- disable Copilot.lua's ghost text and panel (they'd conflict with cmp)
+		suggestion = { enabled = false }, --  [oai_citation:0‡GitHub](https://github.com/zbirenbaum/copilot-cmp)
+		panel = { enabled = false },
+
+		-- ask the LSP to return up to 3 inline suggestions at once
+		server_opts_overrides = {
+			settings = {
+				advanced = {
+					inlineSuggestCount = 3, --  [oai_citation:1‡GitHub](https://github.com/zbirenbaum/copilot.lua)
+				},
 			},
-			-- hide suggestion text when completion menu is open (to avoid overlap)
-			hide_during_completion = true,
 		},
-		panel = { enabled = false, auto_refresh = true }, -- disable Copilot panel (not used in this setup)
+
+		-- your existing filetype overrides…
 		filetypes = {
-			markdown = true, -- enable in Markdown (disabled by default) [oai_citation:4‡neovimcraft.com](https://neovimcraft.com/plugin/zbirenbaum/copilot.lua/#:~:text=require%28,env%20files%20return%20false)
-			yaml = false, -- (example) keep disabled for YAML files
-			-- add any other filetypes overrides if needed; by default most languages are enabled
+			markdown = true,
+			yaml = true,
+			-- etc…
 		},
-		-- Optionally, set NodeJS path if your system Node is < 20:
-		-- copilot_node_command = "/path/to/node>=20"  [oai_citation:5‡neovimcraft.com](https://neovimcraft.com/plugin/zbirenbaum/copilot.lua/#:~:text=Use%20this%20field%20to%20provide,must%20be%2020%20or%20newer)
 	},
 }
+
 -- return {
+-- 	-- 1. Copilot.lua plugin (GitHub Copilot client)
 -- 	"zbirenbaum/copilot.lua",
--- 	event = "InsertEnter",
+-- 	event = "InsertEnter", -- delay loading until Insert mode for efficiency [oai_citation:1‡neovimcraft.com](https://neovimcraft.com/plugin/zbirenbaum/copilot.lua/#:~:text=Because%20the%20copilot%20server%20takes,For%20example)
+-- 	cmd = "Copilot", -- also allow manual `:Copilot` commands
+-- 	build = ":Copilot auth", -- (optional) automatically authenticate on first install
 -- 	opts = {
--- 		panel = { enabled = true },
 -- 		suggestion = {
 -- 			enabled = true,
--- 			auto_trigger = true,
+-- 			auto_trigger = true, -- auto-show suggestions as you type [oai_citation:2‡lazyvim.github.io](https://lazyvim.github.io/extras/ai/copilot#:~:text=opts%20%3D%20,)
+-- 			debounce = 75,
 -- 			keymap = {
--- 				accept = "<Tab>",
--- 				next = "<C-]>",
--- 				prev = "<S-Tab>",
--- 				dismiss = "<C-e>",
+-- 				accept = "<Tab>", -- use <Tab> to accept suggestion [oai_citation:3‡gist.github.com](https://gist.github.com/haandol/c2ff89706fa8af6edf1bb2bb0c1fe3ba#:~:text=suggestion%20%3D%20,)
+-- 				accept_word = false,
+-- 				accept_line = false,
+-- 				next = "<M-]>",
+-- 				prev = "<M-[>",
+-- 				dismiss = "<C-]>",
 -- 			},
--- 			-- keymap = {
--- 			-- 	accept = "<C-l>", -- accept suggestion
--- 			-- 	next = "<C-]>", -- next suggestion
--- 			-- 	prev = "<C-[>", -- previous suggestion
--- 			-- 	dismiss = "<C-_>", -- dismiss suggestion
--- 			-- },
+-- 			-- hide suggestion text when completion menu is open (to avoid overlap)
+-- 			hide_during_completion = true,
 -- 		},
--- 		filetypes = { ["*"] = true },
+-- 		server_opts_overrides = {
+-- 			settings = {
+-- 				advanced = {
+-- 					inlineSuggestCount = 3, -- e.g. get up to 3 suggestions  [oai_citation:1‡GitHub](https://github.com/zbirenbaum/copilot.lua)
+-- 				},
+-- 			},
+-- 		},
+-- 		panel = { enabled = false, auto_refresh = true }, -- disable Copilot panel (not used in this setup)
+-- 		filetypes = {
+-- 			markdown = true, -- enable in Markdown (disabled by default) [oai_citation:4‡neovimcraft.com](https://neovimcraft.com/plugin/zbirenbaum/copilot.lua/#:~:text=require%28,env%20files%20return%20false)
+-- 			yaml = true, -- (example) keep disabled for YAML files
+-- 			-- add any other filetypes overrides if needed; by default most languages are enabled
+-- 		},
+-- 		-- Optionally, set NodeJS path if your system Node is < 20:
+-- 		-- copilot_node_command = "/path/to/node>=20"  [oai_citation:5‡neovimcraft.com](https://neovimcraft.com/plugin/zbirenbaum/copilot.lua/#:~:text=Use%20this%20field%20to%20provide,must%20be%2020%20or%20newer)
 -- 	},
 -- }
--- dot_config/nvim/lua/fuentastic/plugins/nvim-cmp.lua
+++ dot_config/nvim/lua/fuentastic/plugins/nvim-cmp.lua
@@ -1,39 +1,17 @@
+-- your nvim-cmp config file
 return {
 	"hrsh7th/nvim-cmp",
 	event = "InsertEnter",
 	dependencies = {
-		{
-			"L3MON4D3/LuaSnip",
-			build = (function()
-				if vim.fn.has("win32") == 1 or vim.fn.executable("make") == 0 then
-					return
-				end
-				return "make install_jsregexp"
-			end)(),
-			dependencies = {
-				-- `friendly-snippets` contains a variety of premade snippets.
-				--    See the README about individual language/framework/plugin snippets:
-				--    https://github.com/rafamadriz/friendly-snippets
-				-- {
-				--   'rafamadriz/friendly-snippets',
-				--   config = function()
-				--     require('luasnip.loaders.from_vscode').lazy_load()
-				--   end,
-				-- },
-			},
-		},
+		"L3MON4D3/LuaSnip",
 		"saadparwaiz1/cmp_luasnip",
-
-		-- Adds other completion capabilities.
-		--  nvim-cmp does not ship with all sources by default. They are split
-		--  into multiple repos for maintenance purposes.
 		"hrsh7th/cmp-nvim-lsp",
 		"hrsh7th/cmp-path",
 	},
 	config = function()
-		-- See `:help cmp`
 		local cmp = require("cmp")
 		local luasnip = require("luasnip")
+
 		luasnip.config.setup({})
 
 		cmp.setup({
@@ -42,78 +20,188 @@ return {
 					luasnip.lsp_expand(args.body)
 				end,
 			},
-			completion = { completeopt = "menu,menuone,noinsert" },
+
+			-- show the docs preview with a border
+			window = {
+				documentation = cmp.config.window.bordered(), --  [oai_citation:2‡GitHub](https://github.com/hrsh7th/nvim-cmp/issues/1029?utm_source=chatgpt.com)
+			},
 
 			mapping = {
-				["<C-Space>"] = require("cmp").mapping.complete(), -- manually open completion menu
-				["<C-n>"] = require("cmp").mapping.select_next_item({ behavior = require("cmp").SelectBehavior.Insert }),
-				["<C-p>"] = require("cmp").mapping.select_prev_item({ behavior = require("cmp").SelectBehavior.Insert }),
-				["<CR>"] = require("cmp").mapping.confirm({ select = false }), -- confirm selection with Enter
-				["<Tab>"] = require("cmp").mapping(function(fallback)
-					local cmp = require("cmp")
-					local copilot_suggestion = require("copilot.suggestion")
-					if copilot_suggestion.is_visible() then
-						-- If a Copilot inline suggestion is showing, accept it
-						copilot_suggestion.accept()
-					elseif cmp.visible() and require("cmp.utils.misc").has_words_before() then
-						-- If completion menu is open and cursor is after a character, select next item [oai_citation:14‡github.com](https://github.com/zbirenbaum/copilot-cmp#:~:text=mapping%20%3D%20%7B%20%5B,end%20end%29%2C)
+				["<C-Space>"] = cmp.mapping.complete(),
+				["<C-n>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
+				["<C-p>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
+				["<CR>"] = cmp.mapping.confirm({ select = false }),
+				["<Tab>"] = cmp.mapping(function(fallback)
+					if cmp.visible() then
 						cmp.select_next_item({ behavior = cmp.SelectBehavior.Select })
-					elseif require("luasnip").expand_or_jumpable() then
-						-- If inside a snippet, jump to next snippet placeholder
-						require("luasnip").expand_or_jump()
+					elseif luasnip.expand_or_jumpable() then
+						luasnip.expand_or_jump()
 					else
-						fallback() -- otherwise, insert a literal tab (indent)
+						fallback()
 					end
 				end, { "i", "s" }),
-				["<S-Tab>"] = require("cmp").mapping(function(fallback) -- Shift-Tab for snippet jump backwards
-					if require("luasnip").jumpable(-1) then
-						require("luasnip").jump(-1)
+				["<S-Tab>"] = cmp.mapping(function(fallback)
+					if luasnip.jumpable(-1) then
+						luasnip.jump(-1)
 					else
 						fallback()
 					end
 				end, { "i", "s" }),
 			},
+
 			sources = {
-				{ name = "copilot" }, -- GitHub Copilot source (AI suggestions)
-				{ name = "nvim_lsp" }, -- LSP completions
-				{ name = "luasnip" }, -- snippet completions
-				{ name = "buffer" },
+				{ name = "copilot" },
+				{ name = "nvim_lsp" },
+				{ name = "luasnip" },
 				{ name = "path" },
-				{ name = "lazydev" }, -- Lua dev completions (if editing Neovim config)
+				{ name = "buffer" },
+				{ name = "lazydev" },
 			},
+
 			sorting = {
-				priority_weight = 2, -- boost the effect of source priority [oai_citation:11‡github.com](https://github.com/zbirenbaum/copilot-cmp#:~:text=One%20custom%20comparitor%20for%20sorting,stuck%20below%20poor%20copilot%20matches)
+				priority_weight = 2,
 				comparators = {
-					require("copilot_cmp.comparators").prioritize, -- custom: prioritize Copilot [oai_citation:12‡github.com](https://github.com/zbirenbaum/copilot-cmp#:~:text=One%20custom%20comparitor%20for%20sorting,stuck%20below%20poor%20copilot%20matches)
-					require("cmp.config.compare").exact, -- ensure exact LSP matches aren’t overshadowed [oai_citation:13‡github.com](https://github.com/zbirenbaum/copilot-cmp#:~:text=One%20custom%20comparitor%20for%20sorting,stuck%20below%20poor%20copilot%20matches)
-					-- … (include other default comparators)
-					require("cmp.config.compare").score,
-					require("cmp.config.compare").recently_used,
-					require("cmp.config.compare").locality,
-					require("cmp.config.compare").kind,
-					require("cmp.config.compare").sort_text,
-					require("cmp.config.compare").length,
-					require("cmp.config.compare").order,
+					require("copilot_cmp.comparators").prioritize,
+					cmp.config.compare.exact,
+					cmp.config.compare.score,
+					cmp.config.compare.recently_used,
+					cmp.config.compare.locality,
+					cmp.config.compare.kind,
+					cmp.config.compare.sort_text,
+					cmp.config.compare.length,
+					cmp.config.compare.order,
 				},
 			},
 		})
 
+		-- optionally also enable border in command-line mode
 		cmp.setup.cmdline("/", {
 			mapping = cmp.mapping.preset.cmdline(),
-			sources = {
-				{ name = "buffer" },
-			},
+			sources = { { name = "buffer" } },
 		})
-
 		cmp.setup.cmdline(":", {
 			mapping = cmp.mapping.preset.cmdline(),
-			sources = cmp.config.sources({
-				{ name = "path" },
-			}, {
-				{ name = "cmdline", option = {
-					ignore_cmds = { "Man", "!" },
-				} },
-			}),
+			sources = cmp.config.sources({ { name = "path" } }, { { name = "cmdline" } }),
 		})
 	end,
 }
+
+-- return {
+-- 	"hrsh7th/nvim-cmp",
+-- 	event = "InsertEnter",
+-- 	dependencies = {
+-- 		{
+-- 			"L3MON4D3/LuaSnip",
+-- 			build = (function()
+-- 				if vim.fn.has("win32") == 1 or vim.fn.executable("make") == 0 then
+-- 					return
+-- 				end
+-- 				return "make install_jsregexp"
+-- 			end)(),
+-- 			dependencies = {
+-- 				-- `friendly-snippets` contains a variety of premade snippets.
+-- 				--    See the README about individual language/framework/plugin snippets:
+-- 				--    https://github.com/rafamadriz/friendly-snippets
+-- 				-- {
+-- 				--   'rafamadriz/friendly-snippets',
+-- 				--   config = function()
+-- 				--     require('luasnip.loaders.from_vscode').lazy_load()
+-- 				--   end,
+-- 				-- },
+-- 			},
+-- 		},
+-- 		"saadparwaiz1/cmp_luasnip",
+--
+-- 		-- Adds other completion capabilities.
+-- 		--  nvim-cmp does not ship with all sources by default. They are split
+-- 		--  into multiple repos for maintenance purposes.
+-- 		"hrsh7th/cmp-nvim-lsp",
+-- 		"hrsh7th/cmp-path",
+-- 	},
+-- 	config = function()
+-- 		-- See `:help cmp`
+-- 		local cmp = require("cmp")
+-- 		local luasnip = require("luasnip")
+-- 		luasnip.config.setup({})
+--
+-- 		cmp.setup({
+-- 			snippet = {
+-- 				expand = function(args)
+-- 					luasnip.lsp_expand(args.body)
+-- 				end,
+-- 			},
+-- 			completion = { completeopt = "menu,menuone,noinsert" },
+--
+-- 			mapping = {
+-- 				["<C-Space>"] = require("cmp").mapping.complete(), -- manually open completion menu
+-- 				["<C-n>"] = require("cmp").mapping.select_next_item({ behavior = require("cmp").SelectBehavior.Insert }),
+-- 				["<C-p>"] = require("cmp").mapping.select_prev_item({ behavior = require("cmp").SelectBehavior.Insert }),
+-- 				["<CR>"] = require("cmp").mapping.confirm({ select = false }), -- confirm selection with Enter
+-- 				["<Tab>"] = require("cmp").mapping(function(fallback)
+-- 					local cmp = require("cmp")
+-- 					local copilot_suggestion = require("copilot.suggestion")
+-- 					if copilot_suggestion.is_visible() then
+-- 						-- If a Copilot inline suggestion is showing, accept it
+-- 						copilot_suggestion.accept()
+-- 					elseif cmp.visible() and require("cmp.utils.misc").has_words_before() then
+-- 						-- If completion menu is open and cursor is after a character, select next item [oai_citation:14‡github.com](https://github.com/zbirenbaum/copilot-cmp#:~:text=mapping%20%3D%20%7B%20%5B,end%20end%29%2C)
+-- 						cmp.select_next_item({ behavior = cmp.SelectBehavior.Select })
+-- 					elseif require("luasnip").expand_or_jumpable() then
+-- 						-- If inside a snippet, jump to next snippet placeholder
+-- 						require("luasnip").expand_or_jump()
+-- 					else
+-- 						fallback() -- otherwise, insert a literal tab (indent)
+-- 					end
+-- 				end, { "i", "s" }),
+-- 				["<S-Tab>"] = require("cmp").mapping(function(fallback) -- Shift-Tab for snippet jump backwards
+-- 					if require("luasnip").jumpable(-1) then
+-- 						require("luasnip").jump(-1)
+-- 					else
+-- 						fallback()
+-- 					end
+-- 				end, { "i", "s" }),
+-- 			},
+-- 			sources = {
+-- 				{ name = "copilot" }, -- GitHub Copilot source (AI suggestions)
+-- 				{ name = "nvim_lsp" }, -- LSP completions
+-- 				{ name = "luasnip" }, -- snippet completions
+-- 				{ name = "buffer" },
+-- 				{ name = "path" },
+-- 				{ name = "lazydev" }, -- Lua dev completions (if editing Neovim config)
+-- 			},
+-- 			sorting = {
+-- 				priority_weight = 2, -- boost the effect of source priority [oai_citation:11‡github.com](https://github.com/zbirenbaum/copilot-cmp#:~:text=One%20custom%20comparitor%20for%20sorting,stuck%20below%20poor%20copilot%20matches)
+-- 				comparators = {
+-- 					require("copilot_cmp.comparators").prioritize, -- custom: prioritize Copilot [oai_citation:12‡github.com](https://github.com/zbirenbaum/copilot-cmp#:~:text=One%20custom%20comparitor%20for%20sorting,stuck%20below%20poor%20copilot%20matches)
+-- 					require("cmp.config.compare").exact, -- ensure exact LSP matches aren’t overshadowed [oai_citation:13‡github.com](https://github.com/zbirenbaum/copilot-cmp#:~:text=One%20custom%20comparitor%20for%20sorting,stuck%20below%20poor%20copilot%20matches)
+-- 					-- … (include other default comparators)
+-- 					require("cmp.config.compare").score,
+-- 					require("cmp.config.compare").recently_used,
+-- 					require("cmp.config.compare").locality,
+-- 					require("cmp.config.compare").kind,
+-- 					require("cmp.config.compare").sort_text,
+-- 					require("cmp.config.compare").length,
+-- 					require("cmp.config.compare").order,
+-- 				},
+-- 			},
+-- 		})
+--
+-- 		cmp.setup.cmdline("/", {
+-- 			mapping = cmp.mapping.preset.cmdline(),
+-- 			sources = {
+-- 				{ name = "buffer" },
+-- 			},
+-- 		})
+--
+-- 		cmp.setup.cmdline(":", {
+-- 			mapping = cmp.mapping.preset.cmdline(),
+-- 			sources = cmp.config.sources({
+-- 				{ name = "path" },
+-- 			}, {
+-- 				{ name = "cmdline", option = {
+-- 					ignore_cmds = { "Man", "!" },
+-- 				} },
+-- 			}),
+-- 		})
+-- 	end,
+-- }
