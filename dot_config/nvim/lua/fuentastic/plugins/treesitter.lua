a23a47bba821444e067ad5feb7a9be03be69d2d7
Miguel Fuentes <miguel@midgetgiraffe.com>
2026-04-04 01:12:11 +0200

    fix: markdown issue

--- dot_config/nvim/lua/fuentastic/plugins/treesitter.lua
+++ dot_config/nvim/lua/fuentastic/plugins/treesitter.lua
@@ -1,7 +1,7 @@
 return {
   'nvim-treesitter/nvim-treesitter',
-  build = ':TSUpdate',
-  main = 'nvim-treesitter.configs',
+  branch = 'main',
+  build = ':TSUpdate bash c diff html lua luadoc markdown markdown_inline query vim vimdoc',
   opts = {
     ensure_installed = {
       'bash',
@@ -16,11 +16,14 @@ return {
       'vim',
       'vimdoc',
     },
-    auto_install = true,
+    auto_install = false,
     highlight = {
       enable = true,
       additional_vim_regex_highlighting = { 'ruby' },
     },
     indent = { enable = true, disable = { 'ruby' } },
   },
+  config = function(_, opts)
+    require('nvim-treesitter').setup(opts)
+  end,
 }
